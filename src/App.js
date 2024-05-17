a50484f52e6acfaed997856b9239901ec53f9a7e
Parastoo Mozhdeh <parastoo.mozhdeh@gmail.com>
2024-05-16 23:13:43 -0700

    Upload React-Form  project on Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const fileRef = useRef();
+
+  const handleVideoUpload = (event) => {
+    event.preventDefault();
+
+    const file = fileRef.current.files[0];
+    if (!file) {
+      alert("No video file selected");
+      return;
+    }
+
+    const humanFileSize = (size) => {
+      let i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+      return (
+        +(size / Math.pow(1024, i)).toFixed(2) * 1 +
+        " " +
+        ["B", "kB", "MB", "GB", "TB"][i]
+      );
+    };
+
+    localStorage.setItem("fileName", file.name);
+    localStorage.setItem("fileSize", file.size);
+
+    console.log("File Name:", file.name);
+    console.log("File Size:", file.size);
+    console.log("Human File Size:", humanFileSize(file.size));
+
+    const videoUrl = URL.createObjectURL(file);
+
+    const videoElement = document.getElementById("video");
+    videoElement.src = videoUrl;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="form" onSubmit={handleVideoUpload}>
+      <label className="file" htmlFor="input">
+        Upload a video file
+      </label>
+
+      <input
+        className="input-video"
+        type="file"
+        accept="video/*"
+        ref={fileRef}
+      />
+      <video id="video" width="420" height="220"></video>
+
+      <button className="button-wrapper" type="submit">
+        <span>Submit</span>
+      </button>
+    </form>
   );
-}
+};
 
 export default App;
